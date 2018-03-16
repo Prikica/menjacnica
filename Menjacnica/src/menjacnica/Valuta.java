2a037609eec2d100d327888d39e3f23bb21d25b5
Prikica <dejannn06@gmail.com>
2018-03-16 01:33:11 +0100

    Izgenerisane hashCode,equals i toString metode

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -32,4 +32,51 @@ public class Valuta {
 		this.kursnaLista = kursnaLista;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursnaLista == null) ? 0 : kursnaLista.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursnaLista == null) {
+			if (other.kursnaLista != null)
+				return false;
+		} else if (!kursnaLista.equals(other.kursnaLista))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		String tekst = "";
+		tekst += "Valuta [" + skraceniNaziv + "] " + naziv + "\n";
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			tekst += kursnaLista.get(i).toString();
+		}
+		return tekst;
+	}
+
 }
