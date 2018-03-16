c2bf57aeaac262562191eb15c24711be06f9e2de
Prikica <dejannn06@gmail.com>
2018-03-16 16:53:00 +0100

    Implementirana klasa Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,61 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.MenjacnicaInterfejs;
 
-public class Menjacnica implements MenjacnicaInterfejs{
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKursValute(Valuta valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
-		
+		if (valuta == null || kurs == null) {
+			throw new RuntimeException("Neispravni parametri!");
+		}
+
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).equals(valuta)) {
+				valute.get(i).getKursnaLista().add(kurs);
+			}
+		}
+
 	}
 
 	@Override
 	public void obrisiKursValute(GregorianCalendar datumZaBrisanje, Valuta valuta) {
-		// TODO Auto-generated method stub
-		
+		if (datumZaBrisanje == null || valuta == null) {
+			throw new RuntimeException("Neispravni parametri");
+		}
+
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).equals(valuta)) {
+				for (int j = 0; j < valute.get(i).getKursnaLista().size(); j++) {
+					if (valute.get(i).getKursnaLista().get(j).getDatum().equals(datumZaBrisanje))
+						valute.get(i).getKursnaLista().remove(valute.get(i).getKursnaLista().get(j));
+				}
+			}
+
+		}
+
 	}
 
 	@Override
 	public Kurs nadjiIVratiKursValute(GregorianCalendar datum, Valuta valuta) {
-		// TODO Auto-generated method stub
+		if (datum == null || valuta == null) {
+			throw new RuntimeException("Neispravni parametri");
+		}
+
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).equals(valuta)) {
+				for (int j = 0; j < valute.get(i).getKursnaLista().size(); j++) {
+					if (valute.get(i).getKursnaLista().get(j).getDatum().equals(datum))
+						return valute.get(i).getKursnaLista().get(j);
+				}
+			}
+
+		}
 		return null;
 	}
 
