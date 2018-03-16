1e9aac59dd08a8c0536cdf248ad6a1d17d0239f5
Prikica <dejannn06@gmail.com>
2018-03-16 16:14:59 +0100

    Kreirana klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import specifikacijainterfejsa.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
+
+	@Override
+	public void dodajKursValute(Valuta valuta, Kurs kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKursValute(GregorianCalendar datumZaBrisanje, Valuta valuta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs nadjiIVratiKursValute(GregorianCalendar datum, Valuta valuta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
