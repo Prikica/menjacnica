85b0d478cf513f6c6696000efc794fb005d3e0a1
Prikica <dejannn06@gmail.com>
2018-03-16 17:14:27 +0100

    Implementirana metoda dodajKursValute

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,21 +1,31 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.MenjacnicaInterfejs;
 
-public class Menjacnica implements MenjacnicaInterfejs{
+public class Menjacnica implements MenjacnicaInterfejs {
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKursValute(Valuta valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
-		
+		if (valuta == null || kurs == null) {
+			throw new RuntimeException("Neispravni parametri!");
+		}
+
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getSkraceniNaziv().equals(valuta.getSkraceniNaziv())) {
+				valute.get(i).getKursnaLista().add(kurs);
+			}
+		}
+
 	}
 
 	@Override
 	public void obrisiKursValute(GregorianCalendar datumZaBrisanje, Valuta valuta) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
